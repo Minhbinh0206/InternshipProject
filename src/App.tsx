61a068083ec043d2d1058cd3349760322dbcffb8
Minhbinh0206 <haminh4235@gmail.com>
2025-07-01 14:29:23 +0700

    feature/CustomButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
-import CodeBuilder from './components/CodeBuilder/CodeBuider'
+import CustomButton from './components/CustomButton/CustomButton'
+import { PlusOutlined } from '@ant-design/icons';
+
 
 function App() {
 
   return (
     <>
       <div style={{ padding: 40 }}>
-        <CodeBuilder />
+        <CustomButton
+          variant="blue"
+          layout="iconFirst"
+          icon={<PlusOutlined />}
+          text="Add part"
+        />
+
+        <CustomButton
+          variant="white"
+          layout="textFirst"
+          icon={<PlusOutlined />}
+          text="Create"
+        />
+
+        <CustomButton variant="black" text="Delete" />
+
+        <CustomButton variant="red" text="Remove" disabled />
       </div>
     </>
   )
